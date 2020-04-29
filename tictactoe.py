0c683d58991ade083f9af83f0cf2c58e61ae388a
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2020-04-29 15:17:54 +0200

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,64 @@
+import pygame
+pygame.font.init()
+
+Width, Height = 750, 750
+
+Win = pygame.display.set_mode((Width, Height))
+Bg = (0,255,255)
+
+clock = pygame.time.Clock()
+FPS = 80
+
+class Grid():
+    def __init__(self):
+        self.grid = [((0, Height//4), (Width, Height//4)), ((0, (2*Height)//4), (Width, (2*Height)//4)), ((0, (3*Height)//4), (Width, (3*Height)//4)), ((Width//4, 0), (Width//4, Height)), (((2*Width)//4, 0), ((2*Width)//4, Height)), (((3*Width)//4,0), ((3*Width)//4, Height))]
+
+        self.grid2 = [[0 for x in range(4)] for y in range(4)]
+
+
+    def draw(self, window):
+        for line in self.grid:
+            pygame.draw.line(window, (255, 255, 255), line[0], line[1], 2)
+
+    def get_cell_value(self, x,y):
+        return self.grid2[y][x]
+
+    def set_cell_value(self, x, y, value):
+        self.grid2[x][y] = value
+
+    def print_grid(self):
+        for row in self.grid2:
+            print(row)
+
+Grid = Grid()
+Grid.set_cell_value(2, 1, 'x')
+
+
+def redraw_window():
+    Win.fill(Bg)
+
+    Grid.draw(Win)
+    pygame.display.flip()
+
+
+
+def ui():
+    run = True
+    Grid.print_grid()
+    while run:
+        clock.tick(FPS)
+
+        redraw_window()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #print("Yes !")
+
+                if pygame.mouse.get_pressed()[0]:
+                    pos = pygame.mouse.get_pos()
+
+                    print(pos[0] // (Width // 4), pos[1] // (Height // 4))
+
+
+ui()
