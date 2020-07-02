82ce23679fd132281512bfd2bb34356c4f93d7af
MrRex <lucas.colas13@gmail.com>
2020-07-02 13:48:19 +0200

    upgrade

--- AI_TicTacToe_3x3.py
+++ AI_TicTacToe_3x3.py
@@ -12,7 +12,7 @@ Win = pygame.display.set_mode((Width, Height))
 
 
 Cross = pygame.transform.scale(pygame.image.load(os.path.join("assets", "cross.png")), (Width//3, Height//3))
-Cirlce = pygame.transform.scale(pygame.image.load(os.path.join("assets", "circle.png")), (Width//3, Height//3))
+Circle = pygame.transform.scale(pygame.image.load(os.path.join("assets", "circle.png")), (Width//3, Height//3))
 
 Bg = (0,0,0)
 Clock = pygame.time.Clock()
@@ -39,16 +39,27 @@ def print_board(board):
     for row in board:
         print(row)
 
+def draw_board(board):
+    pass
+
+def redraw_window(board):
+
+    Win.fill(Bg)
+    pygame.display.update()
+
 board = create_board()
 
 def main():
+    global board
     run = True
     green = (0,255,0,0)
 
     while run:
+        Clock.tick(FPS)
+        redraw_window(board)
         fill(Circle, green)
-
-        for event in event.pygame.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
-            
+
+main()
