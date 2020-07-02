8a26fb5efe20777228a081807722e3825552e6ad
MrRex <lucas.colas13@gmail.com>
2020-07-02 13:19:24 +0200

    upgrade

--- AI_TicTacToe_3x3.py
+++ AI_TicTacToe_3x3.py
@@ -2,3 +2,35 @@ import pygame
 import random
 import math
 from math import inf as infinity
+import sys
+import os
+
+pygame.font.init()
+
+Width, Height = 750,750
+Win = pygame.display.set_mode((Width, Height))
+
+
+Cross = pygame.transform.scale(pygame.image.load(os.path.join("assets", "cross.png")), (Width//3, Height//3))
+Cirlce = pygame.transform.scale(pygame.image.load(os.path.join("assets", "circle.png")), (Width//3, Height//3))
+
+Bg = (0,0,0)
+Clock = pygame.time.Clock()
+
+FPS = 80
+
+AI = 1
+Human = -1
+
+def create_board():
+    new_board = [[0 for i in range(3)] for j in range(3)]
+    return new_board
+
+def print_board(board):
+    #print(board)
+    for row in board:
+        print(row)
+
+
+board = create_board()
+print_board(board)
