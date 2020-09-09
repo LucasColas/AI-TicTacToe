b43af2359266a20822c9ea0408fc684162afccc3
LucasColas <lucas.colas13@gmail.com>
2020-09-09 20:42:02 +0200

    update

--- RL/AI_TicTacToe_3x3_ABP.py
+++ RL/AI_TicTacToe_3x3_ABP.py
@@ -34,6 +34,20 @@ Human = -1
 
 FPS = 80
 
+class QLearning():
+    def __init__(self, lr, gamma, episodes, Epsilon, Min_Epsilon, Decay):
+        self.lr = lr
+        self.gamma = gamma
+        self.episodes = episodes
+        self.Epsilon = Epsilon
+        self.Min_Epsilon = Min_Epsilon
+        self.Decay = Decay
+
+    def Q_Table(self):
+        Q_Table = [0 for i in range(9)]
+        return Q_Table
+        
+
 def fill(surface, color):
     w, h = surface.get_size()
     r, g, b, _ = color
--- RL/TTT_RL.py
+++ RL/TTT_RL.py
@@ -16,19 +16,7 @@ Circle = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "circle
 
 clock = pygame.time.Clock()
 
-class QLearning():
-    def __init__(self, lr, gamma, episodes, Epsilon, Min_Epsilon, Decay):
-        self.lr = lr
-        self.gamma = gamma
-        self.episodes = episodes
-        self.Epsilon = Epsilon
-        self.Min_Epsilon = Min_Epsilon
-        self.Decay = Decay
 
-    def Q_Table(self):
-        Q_Table = [0 for i in range(9)]
-        return Q_Table
-        
 
 def fill(surface, color):
     w,h = surface.get_size()
